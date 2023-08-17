813358b12a4168ef2bac0612409788bd8cec0eaf
Igor Nagrebelnyi <nagrebelnyi.igor@gmail.com>
2023-08-17 16:04:00 +0300

    commit

---
+++ src/home/helpers/sum.js
@@ -0,0 +1,7 @@
+// function sum(a, b) {
+//   return a ** b;
+// }
+
+// export { sum };
+value = 5;
+export default value;
--- src/index.js
+++ src/index.js
@@ -1,7 +1,29 @@
-let count = 0;
-for (let i = 0; i < 10; i += 1) {
-  count += 1;
-  console.log(count);
-}
+// let count = 0;
+// for (let i = 0; i < 10; i += 1) {
+//   count += 1;
+//   console.log(count);
+// }
 
-console.log('hello world');
+// console.log('hello world');
+
+// import { add, sum } from './home/helpers/add';
+
+// console.log(add(5, 12));
+
+// import qwerty from './home/helpers/add';
+// console.log(qwerty(5, 12));
+
+// import * as ESmodule from './home/helpers/add';
+// console.log(ESmodule);
+// console.log(ESmodule.value);
+// console.log(sum(23, 11));
+
+// import { sum } from './home/helpers/add';
+// import { sum as sumSum } from './home/helpers/sum';
+
+// console.log(sum(10, 2));
+// console.log(sumSum(10, 2));
+
+import value from './home/helpers/sum';
+
+console.log(count);
