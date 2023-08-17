b7582dfcffec1849fe8c67f0bdc352e0362e7831
IgorN1989 <107757685+IgorN1989@users.noreply.github.com>
2023-08-17 08:58:54 +0000

    Deploying to gh-pages from @ IgorN1989/goit-js-hw-08-2@2a01bfece033c917f469fed361fb66f936787a7c 🚀

---
+++ index.118adbc1.js
@@ -0,0 +1,2 @@
+!function(){console.log("hello world"),count=0;for(var o=0;o<10;o+=1)count+=1,console.log(count)}();
+//# sourceMappingURL=index.118adbc1.js.map
---
+++ index.ea083649.js
@@ -0,0 +1,2 @@
+console.log("hello world"),count=0;for(let o=0;o<10;o+=1)count+=1,console.log(count);
+//# sourceMappingURL=index.ea083649.js.map
